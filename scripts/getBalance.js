68b41c17f49aa9bd534db07c440fbc1c2aaf8080
Ritamkundu21 <randomritam@gmail.com>
2024-07-09 18:34:52 +0530

    Update getBalance.js

--- scripts/getBalance.js
+++ scripts/getBalance.js
@@ -8,7 +8,7 @@
 require('dotenv').config();
 const { ethers } = require("hardhat");
 
-const nftAddress = "0x5FF12245A118fb3067267C92E861D8F8DA648106";
+const nftAddress = "0xE8cD2EC930b9920c38B81Ac7f10eb76Ea9da3511";
 const networkAddress = 'https://polygon-amoy-bor-rpc.publicnode.com';
 const privateKey = process.env.PRIVATE_KEY;
 const provider = new ethers.providers.JsonRpcProvider(networkAddress);
